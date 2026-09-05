242b88ddcef275acd48dd575c78c8697b9665ddd
Juloc <juli.hiresch@gmail.com>
2026-09-05 13:48:15 +0200

    chore: include Enable Banking profiles in model snapshot

--- src/FullWorth.Backend/Migrations/FullWorthDbContextModelSnapshot.cs
+++ src/FullWorth.Backend/Migrations/FullWorthDbContextModelSnapshot.cs
@@ -16,6 +16,7 @@ partial class FullWorthDbContextModelSnapshot : ModelSnapshot
 #pragma warning disable 612, 618
         new AddNotificationDedups().BuildSnapshotBaseline(modelBuilder);
         FullWorthDbContextSnapshotDeltaV20260902.Apply(modelBuilder);
+        EnableBankingProfilesSnapshot.Apply(modelBuilder);
 #pragma warning restore 612, 618
     }
 }
