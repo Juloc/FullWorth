d426a79313d52a026e5fc82d78bd83734c2987c3
Juloc <juli.hiresch@gmail.com>
2026-09-06 21:05:39 +0200

    chore(admin): update auth snapshot

--- src/FullWorth.Web/Migrations/AuthDbContextModelSnapshot.cs
+++ src/FullWorth.Web/Migrations/AuthDbContextModelSnapshot.cs
@@ -13,7 +13,7 @@ partial class AuthDbContextModelSnapshot : ModelSnapshot
     protected override void BuildModel(ModelBuilder modelBuilder)
     {
 #pragma warning disable 612, 618
-        AuthMigrationModelAccountDeletion.Build(modelBuilder);
+        AuthMigrationModelAdmin.Build(modelBuilder);
 #pragma warning restore 612, 618
     }
 }
